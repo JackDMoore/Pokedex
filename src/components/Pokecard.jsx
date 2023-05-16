5260c502cdbe21bdabda77dfb7e47964aaa5bf08
George Hill <georgeophill@outlook.com>
2023-05-16 12:44:33 +0100

    setup

---
+++ src/components/Pokecard.jsx
@@ -0,0 +1,11 @@
+function PokeCard({ title, handle }) {
+
+  return (
+    <div>
+      <div> Title is {title}</div>
+      <div> Handle is {handle}</div>
+    </div>
+  );
+}
+
+export default PokeCard
